a145b0294363f821148fc724f10d0c785206b15a
Madhu Neelaiahgari <madhunee@amazon.com>
2022-11-17 09:21:35 -0600

    updated sg

--- sg.tf
+++ sg.tf
@@ -43,12 +43,6 @@ resource "aws_security_group" "data" {
     to_port     = 0
     protocol    = "-1"
   }
-  ingress {
-    cidr_blocks = ["${var.inspection_vpc_cidr_block}"]
-    from_port   = 6081
-    to_port     = 6081
-    protocol    = "UDP"
-  }
   egress {
     cidr_blocks = ["${var.inspection_vpc_cidr_block}"]
     from_port   = 0
@@ -64,12 +58,6 @@ resource "aws_security_group" "spoke" {
   name        = "spoke_sg"
   description = "Spoke VPC - Security Group"
   vpc_id      = aws_vpc.spoke_vpc.id
-  egress {
-    cidr_blocks = ["${aws_vpc.spoke_vpc.cidr_block}"]
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-  }
   egress {
     cidr_blocks = ["0.0.0.0/0"]
     from_port   = 443
